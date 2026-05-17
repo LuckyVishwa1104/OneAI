dfe8f84e6b4cb4d6c3848ae67cbd4ea742c02837
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-17 14:21:57 +0530

    chore(directory): add ulits directory for constant component of app

---
+++ lib/utils/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Primary and Secondary Color
+  static const Color primary = Color(0xFF7B3FF2);
+  static const Color secondary = Color(0xFF4F6BFF);
+
+}
