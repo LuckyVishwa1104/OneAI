f7eb1af301b0a3ad9597aaedba09606f0b173c19
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-23 22:08:41 +0530

    feat(constant): add constant file with default sizes

---
+++ lib/utils/constants/app_constant.dart
@@ -0,0 +1,15 @@
+class AppConstant {
+  AppConstant._();
+
+  static const double sm = 8.0;
+  static const double md = 12.0;
+  static const double lg = 18.0;
+  static const double xs = 4.0;
+  static const double xxs = 2.0;
+  static const double xl = 20.0;
+  static const double xxl = 24.0;
+  static const double xxxl = 32.0;
+  static const double huge = 40.0;
+  static const double massive = 48.0;
+
+}
