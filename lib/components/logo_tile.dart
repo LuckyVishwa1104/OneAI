ba8591d5f98c2d253cf3d4dd4c1688343d84fd0f
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-28 23:20:31 +0530

    feat(on-tap): wrap the tile componenet with on tap for navigation

--- lib/components/logo_tile.dart
+++ lib/components/logo_tile.dart
@@ -4,39 +4,55 @@ import 'package:one_ai/utils/constants/app_radius.dart' show AppRadius;
 
 class LogoTile extends StatelessWidget {
   final bool isLogo;
+  final bool isCircular;
   final double height;
   final double width;
   final IconData icon;
   final double iconSize;
   final Color iconColor;
+  final VoidCallback? onTap;
   const LogoTile({
     super.key,
     this.isLogo = true,
+    this.isCircular = false,
     this.height = 42,
     this.width = 42,
     this.iconSize = 28,
-    this. iconColor = AppColors.primary,
+    this.iconColor = AppColors.primary,
     required this.icon,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors:
-              isLogo
-                  ? [AppColors.primary, AppColors.secondary]
-                  : [Color(0xFFE0E7FF), Color(0xFFF3E8FF)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors:
+                isLogo
+                    ? [AppColors.primary, AppColors.secondary]
+                    : [Color(0xFFE0E7FF), Color(0xFFF3E8FF)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          shape: isCircular ? BoxShape.circle : BoxShape.rectangle,
+      
+          borderRadius:
+              isCircular
+                  ? null
+                  : (isLogo ? AppRadius.radiusLg : AppRadius.radiusMd),
         ),
-        borderRadius: isLogo ? AppRadius.radiusLg : AppRadius.radiusMd,
+      
+        child: Icon(
+          icon,
+          color: isLogo ? AppColors.appWhite : iconColor,
+          size: iconSize,
+        ),
       ),
-
-      child: Icon(icon, color: isLogo ? AppColors.appWhite : iconColor, size: iconSize),
     );
   }
 }
