f69c1b1562f6ae0453932471b7fc1ae929254409
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-06-05 22:00:33 +0530

    feat(user_message): add required action option and contianer to the user chat message

--- lib/utils/components/chat/user_message_bubble.dart
+++ lib/utils/components/chat/user_message_bubble.dart
@@ -1,23 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:one_ai/model/chat_message_model.dart';
+import 'package:one_ai/utils/components/app_icon.dart';
+import 'package:one_ai/utils/constants/app_border.dart';
+import 'package:one_ai/utils/constants/app_colors.dart';
+import 'package:one_ai/utils/constants/app_constant.dart';
+import 'package:one_ai/utils/constants/app_radius.dart';
+import 'package:one_ai/utils/constants/app_spacing.dart';
 
-class UserMessageBubble extends StatelessWidget {
+class UserMessageBubble extends StatefulWidget {
   final ChatMessageModel message;
-  const UserMessageBubble({super.key, required this.message});
+  final bool isLastUserMessage;
+  final VoidCallback? onEdit;
+  const UserMessageBubble({
+    super.key,
+    required this.message,
+    required this.isLastUserMessage,
+    this.onEdit,
+  });
+
+  @override
+  State<UserMessageBubble> createState() => _UserMessageBubbleState();
+}
+
+class _UserMessageBubbleState extends State<UserMessageBubble> {
+  bool isExpanded = false;
+  static const int maxLines = 15;
+
+  bool _shouldShowExpandButton() {
+    final span = TextSpan(
+      text: widget.message.message,
+      style: const TextStyle(fontSize: 14),
+    );
+
+    final painter = TextPainter(
+      text: span,
+      maxLines: maxLines,
+      textDirection: TextDirection.ltr,
+    );
+
+    painter.layout(maxWidth: 315);
+
+    return painter.didExceedMaxLines;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
       child: Container(
-        constraints: const BoxConstraints(maxWidth: 320),
-        margin: const EdgeInsets.symmetric(vertical: 6),
-        padding: const EdgeInsets.all(12),
+        constraints: const BoxConstraints(maxWidth: 315),
+        margin: const EdgeInsets.symmetric(vertical: AppConstant.sm),
+        padding: AppSpacing.defaultPadding,
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.white12),
-          borderRadius: BorderRadius.circular(18),
+          color: AppColors.surface,
+          borderRadius: AppRadius.radiusXxl,
+          border: AppBorder.defaultBorder,
         ),
-        child: Text(message.message),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.message.message,
+              maxLines: isExpanded ? null : maxLines,
+              overflow: TextOverflow.fade,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (_shouldShowExpandButton())
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        isExpanded = !isExpanded;
+                      });
+                    },
+                    child: AppIcon(
+                      icon: isExpanded ? Icons.expand_less : Icons.expand_more,
+                    ),
+                  ),
+
+                AppSpacing.w4,
+
+                InkWell(
+                  onTap: () {},
+                  child: AppIcon(icon: Icons.copy, size: AppConstant.base),
+                ),
+
+                AppSpacing.w8,
+
+                if (widget.isLastUserMessage)
+                  InkWell(
+                    onTap: widget.onEdit,
+                    child: AppIcon(
+                      icon: Icons.edit_outlined,
+                      size: AppConstant.base,
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
