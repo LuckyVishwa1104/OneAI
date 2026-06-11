042d8e9c167294e736875e02781ed313ca171311
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-06-11 23:51:18 +0530

    feat(route): add route for user profile screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:one_ai/pages/chat/chat_view.dart';
 import 'package:one_ai/pages/counter/counter_view.dart';
 import 'package:one_ai/pages/home/home_view.dart';
+import 'package:one_ai/pages/user_profile/user_profile_view.dart';
 import 'package:one_ai/services/counter_service.dart';
 import 'package:one_ai/services/theme_service.dart';
 import 'package:stacked/stacked_annotations.dart';
@@ -12,7 +13,8 @@ import '../pages/splash_screen/splash_screen_view.dart';
     MaterialRoute(page: HomeView),
     MaterialRoute(page: CounterView),
     MaterialRoute(page: SplashScreenView, initial: true),
-    MaterialRoute(page: ChatView,)
+    MaterialRoute(page: ChatView,),
+    MaterialRoute(page: UserProfileView),
   ],
   dependencies: [
     Singleton(classType: NavigationService),
